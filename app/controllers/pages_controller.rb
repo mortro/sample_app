2874e240e6de5e40751b5af3987aaa2c8a744c19
Mortro <mortro@donamaretto.de>
2011-03-16 09:38:21 +0100

    My first test suite

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,7 @@ class PagesController < ApplicationController
   def about
   end
 
+  def sample
+  end
+
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,17 +2,61 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => " | home")
+    end
   end
+
+
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => " | contact")
+    end
+  end
+
+
+
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      responce.should have_selector("title", :content => " | about")
+    end
+  end
+
+
+
+
+  describe "GET 'sample'" do
+    it "shoult be successful" do
+      get 'sample'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'sample'
+      response.should have_selector("title", :content => " | sample")
     end
   end
 
